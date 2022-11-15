9f876eebf67278908816e124d461c7e7bc14d328
BloodKCrusader <58897753+BloodKCrusader@users.noreply.github.com>
2022-11-14 23:41:07 -0500

    Add GPU Test

---
+++ gpu_test.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
